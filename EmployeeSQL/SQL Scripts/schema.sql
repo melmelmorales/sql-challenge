37cc9f4d095e92f677c251d471162aa41d0fdc54
Melissa Morales <164841786+melmelmorales@users.noreply.github.com>
2024-08-12 14:34:27 -0700

    Update schema.sql

--- EmployeeSQL/SQL Scripts/schema.sql
+++ EmployeeSQL/SQL Scripts/schema.sql
@@ -42,10 +42,7 @@ FROM dept_manager;
 ---- (5) titles table ----
 CREATE TABLE titles (
 	title_id VARCHAR(5) NOT NULL,
-	title VARCHAR(30) NOT NULL,
-	CONSTRAINT pk_titles PRIMARY KEY (
-		title_id
-	)
+	title VARCHAR(30) NOT NULL
 );
 
 SELECT *
